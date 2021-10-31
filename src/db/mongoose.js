f6c5a853e5b05f66e06b94aca7d4ab1286bcb1cb
manishdeadflow <manishdeadflow@gmail.com>
2021-10-31 21:01:46 +0530

    correct

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 
-try{
-  mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-  });
-}catch(e){
-  res.send(e)
-}
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+});
