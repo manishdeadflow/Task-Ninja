19ed7163f221f443ffe2daae8ab4452bde9ad50d
manishdeadflow <manishdeadflow@gmail.com>
2021-11-01 13:56:53 +0530

    mail

--- src/email/account.js
+++ src/email/account.js
@@ -13,16 +13,16 @@ const sendWelcomeMail = (email, name) => {
     from: process.env.USER,
     to: email,
     subject: "Thanks for joining us",
-    text: `welcome to task manager app ,${name}, I hope you your time here ,and let us know if you like this app`
+    html: `<h1>welcome to task manager app ,${name}, I hope you your time here ,and let us know if you like this app</h1>`
   })
 }
 
-const sendCancelingMail = (email,name) =>{
+const sendCancelingMail = (email, name) => {
   transporter.sendMail({
     from: process.enc.USER,
     to: email,
     subject: "Sorry to see you go!",
-    text: `Goodbye ,${name}, I hope to see you back sometime again`
+    html: `<h1>Goodbye ,${name}, I hope to see you back sometime again</h1>`
   })
 }
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -112,8 +112,8 @@ router.patch("/users/me", auth, async (req, res) => {
 router.delete("/users/me", auth, async (req, res) => {
   try {
     await req.user.remove()
-    res.send(req.user);
     sendCancelingMail(req.user.email, req.user.name)
+    res.send(req.user);
   } catch (e) {
     res.status(500).send(e);
   }
